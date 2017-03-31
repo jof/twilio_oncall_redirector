6535f568a1ba32c59d6682ca0cce31fc8db4fdf9
Jonathan Lassoff <jof@planet.com>
2017-03-31 14:36:34 +0000

    Switch to Ruby 2.4.1, aws-sdk, and Pagerduty APIv2.

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 source 'https://rubygems.org'
+ruby '2.4.1'
 
 gem 'sinatra'
 gem 'activesupport'
-gem 'json'
 gem 'rest-client'
-gem 's3'
+#gem 's3'
+gem 'aws-sdk', '~> 2'
 
 group :development do
   gem 'pry'
--- oncall.rb
+++ oncall.rb
@@ -9,7 +9,7 @@ require 'active_support/core_ext/numeric/time'
 require 'active_support/core_ext/time/calculations'
 require 'active_support/core_ext/date/calculations'
 require 'active_support/core_ext/time/acts_like'
-require 's3'
+require 'aws-sdk'
 
 CONFIG = YAML.load_file('config.yaml')
 
@@ -18,56 +18,70 @@ def pd_api_call(method, params = {})
   args[:content_type] = :json
   args[:authorization] = "Token token=#{CONFIG['pagerduty_api_token']}"
   args[:params] = params
+  args[:accept] = "application/vnd.pagerduty+json;version=2"
   return JSON.load(RestClient.get(CONFIG['pagerduty_api_base_url']+method, args))
 end
 
-def who_is_oncall_for(schedule_id)
-  # Search for users on-call in the next 5 minutes, to handle an incoming call.
-  _since = DateTime.now.iso8601
-  _until = 5.minutes.from_now.iso8601
-  schedule = pd_api_call("/schedules/#{schedule_id}/users", {:since => _since, :until => _until})
-  raise Exception.new("There are no users on-call in schedule #{schedule_id}!") unless (schedule["users"] and schedule["users"].length > 0)
-
-  # Get that on-call users' PagerDuty user ID and find their first listed phone
-  # number.
-  on_call_user_id = schedule["users"].first["id"]
-  on_call_user_id
-end
-
 def primary_phone_number_for(user_id)
-  on_call_user_contact_methods = pd_api_call("/users/#{user_id}/contact_methods")["contact_methods"]
-  phone_numbers = on_call_user_contact_methods.select { |method| method["type"] == "phone" }
-  raise Exception.new("There are no phone numbers for user #{user_id} while searching schedule #{schedule_id}!") unless (phone_numbers.length > 0)
+  contact_methods = pd_api_call("/users/#{user_id}/contact_methods")["contact_methods"]
+  phone_numbers = contact_methods.select{|cm| cm["type"] == "phone_contact_method"}
+  if phone_numbers.empty?
+    raise Exception.new("No phone numbers for user #{user_id}!")
+  end
   primary_phone_number = phone_numbers.first
-  primary_phone_number
+  country_code = primary_phone_number["country_code"]
+  subscriber_number = primary_phone_number["address"]
+  e164_number = "+#{country_code}#{subscriber_number}"
+  return e164_number
 end
 
-def publish_call_redirection_for(schedule_id)
-  on_call_user_id = who_is_oncall_for(schedule_id)
-  primary_phone_number = primary_phone_number_for(on_call_user_id)
+def users_oncall_for(escalation_policy_id)
+  resp = pd_api_call("/oncalls?escalation_policy_ids[]=#{escalation_policy_id}")
+  oncalls = resp["oncalls"]
+  oncalls = oncalls.sort {|oncall| oncall["escalation_level"]}
+  user_ids = oncalls.map{|oncall| oncall["user"]["id"]}
+  return user_ids
+end
 
-  country_code = primary_phone_number["country_code"]
-  phone_number = primary_phone_number["phone_number"]
+def publish_call_redirection_for(escalation_policy_id)
+  puts "Publishing for escalation policy #{escalation_policy_id}.."
+  users = users_oncall_for(escalation_policy_id)
+  sorted_escalation_numbers = users.map {|user_id| primary_phone_number_for(user_id)}
 
   # Template a Twilio TwiML document to redirect the call.
   twiml = <<-EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!-- #{DateTime.now.iso8601} -->
 <Response>
-  <Dial timeout="#{CONFIG['redirection_dialing_timeout']}">+#{country_code}#{phone_number}</Dial>
-</Response>
   EOF
+  sorted_escalation_numbers.each do |e164_number|
+    twiml += "  <Dial "
+    twiml += "timeout=\"#{CONFIG['redirection_dialing_timeout']}\" "
+    twiml += "ringTone=\"#{CONFIG['dialing_ringtone']}\" "
+    twiml += ">"
+    twiml += e164_number
+    twiml += "</Dial>"
+    twiml += "\n"
+  end
+  twiml += "</Response>"
+  puts "TwiML doc:"
+  puts twiml
 
   # Upload the file to S3.
-  service = S3::Service.new(:access_key_id => CONFIG['aws_access_key_id'], :secret_access_key => CONFIG['aws_secret_access_key'])
-  bucket = service.buckets.find(CONFIG['s3_bucket'])
-  raise Exception.new("S3 bucket #{CONFIG['s3_bucket']} not found!") unless bucket
-  object = bucket.objects.build("rotation/#{schedule_id}.xml")
-  object.content = twiml
-  object.content_type = 'text/xml'
-  object.save
+  credentials = Aws::Credentials.new(CONFIG['aws_access_key_id'],
+                                     CONFIG['aws_secret_access_key'])
+  s3 = Aws::S3::Client.new(region: "us-gov-west-1", credentials: credentials)
+  bucket_key = "rotation/#{escalation_policy_id}.xml"
+  s3.put_object(
+      bucket: CONFIG['s3_bucket'],
+      key: bucket_key,
+      body: twiml,
+      content_type: "text/xml",
+      acl: "public-read"
+  )
+  puts "Successfully wrote #{bucket_key} to S3."
 end
 
-CONFIG['pagerduty_schedules'].each do |pagerduty_schedule_id|
-  publish_call_redirection_for pagerduty_schedule_id
+CONFIG['pagerduty_escalation_policies'].each do |pagerduty_escalation_policy_id|
+  publish_call_redirection_for pagerduty_escalation_policy_id
 end
